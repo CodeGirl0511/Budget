7c0197efd81c524257f88a8ed2d355536594712c
Swati Patel Torres <swati@Swatis-MBP.fios-router.home>
2020-06-28 11:39:42 -0400

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://localhost/budgethw", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
